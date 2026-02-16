5fbaaf529aca611212e76fdb20235c114277242f
liuzewei <714226301@qq.com>
2026-02-16 15:50:46 +0800

    fix: 修改lint配置

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,14 +1,8 @@
 export default {
   'yisu-hotel/mobile/**/*.{js,jsx,ts,tsx}': [
     'prettier --write',
-    'cd yisu-hotel/mobile && npx eslint --fix',
+    'pnpm --filter mobile exec eslint --fix',
   ],
-  'yisu-hotel/admin/**/*.{ts,tsx}': [
-    'prettier --write',
-    'cd yisu-hotel/admin && npx eslint --fix',
-  ],
-  'server/**/*.{ts,js}': [
-    'prettier --write',
-    'cd server && npx eslint --fix',
-  ],
+  'yisu-hotel/admin/**/*.{ts,tsx}': ['prettier --write', 'pnpm --filter admin exec eslint --fix'],
+  'server/**/*.{ts,js}': ['prettier --write', 'pnpm --filter server exec eslint --fix'],
 }
